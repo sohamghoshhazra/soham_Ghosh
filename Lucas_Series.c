fc3f6172f7fffb5eebd5062fb3c2be6ce9fdf696
Soham Ghosh Hazra <sohamghoshhazra@gmail.com>
2017-05-31 04:45:33 +0530

    Add files via upload

---
+++ Lucas_Series.c
@@ -0,0 +1,19 @@
+//**** Lucas Series using C *****//
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,a=2,b=1,next;
+    printf("Enter The Range=");
+    scanf("%d",&n);
+    printf("%d  %d",a,b);
+    for(i=2;i<n;i++)
+    {
+        next=a+b;
+        a=b;
+        b=next;
+        printf("  %d  ",next);
+}
+}
