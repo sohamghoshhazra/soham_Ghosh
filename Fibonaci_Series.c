d1d602a24d427507bbce5d72b3a501e737df3881
Soham Ghosh Hazra <sohamghoshhazra@gmail.com>
2017-05-31 03:53:51 +0530

    Add files via upload

---
+++ Fibonaci_Series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,a=0,b=1,next;
+    printf("Enter The Range=");
+    scanf("%d",&n);
+    printf("%d  %d",a,b);
+    for(i=2;i<=n;i++)
+    {
+        next=a+b;
+        a=b;
+        b=next;
+        printf("  %d  ",next);
+}
+}
